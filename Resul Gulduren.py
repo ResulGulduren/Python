fadfea93adfcd15f349c7ad4bde5a4c19000d3dc
Sperwer10 <126902916+Sperwer10@users.noreply.github.com>
2023-03-04 00:46:32 +0100

    Add files via upload

--- Resul Gulduren.py
+++ Resul Gulduren.py
@@ -1,29 +1,105 @@
-import random
+import random #Bilgisayardan random tercih yapmasını isteyeceğimiz için random modülünü başlangıçta ekliyoruz.
+
+secenek = ["taş", "kağıt", "makas"] #Random modülünün seçim yapabileceği listemizi oluştuyoruz.
+taş = secenek[0] #Oyuncunun seçim yapabileceği taş, kağıt ve makası string olarak oluştuyoruz.
+kağıt = secenek[1]
+makas = secenek[2]
+senin_skorun = 0 #Skor hesabı yapalım ve oyun belli bir sayıya ulaşınca bitsin istiyorum.
+bilg_skor = 0
+
+#Oyunun kuralları ve oyun ile ilgili bilgileri kullanıcının öğrenmesini hedefliyoruz.
+print("Taş,kağıt,makas oyununa hoş geldiniz. Oyunun kuralları basittir:")
+print("-Taş makası ezer, kağıt taşı örter ve makas kağıdı keser.")
+print("-Başlamak için taş, kağıt veya makas yazıp ENTER tuşuna basmalısınız.")
+print("-Oyunu yeniden başlatmak için r + ENTER tuşuna basınız.")
+print("-Oyundan çıkmak için q + ENTER tuşuna basınız.\n-5 skoruna ilk ulaşan kazanır. Bol şans!!!\n")
 
 while True:
-    print("Taş Kağıt Makas oyununa hoş geldiniz!")
-    print("1. Taş")
-    print("2. Kağıt")
-    print("3. Makas")
-    print("4. Çıkış")
+    bilg_secim = random.choice(secenek) #Bilgisayar secenek kümesinin içinden bir tanesini sececek ve bu bilg_secim olarak tanınacak.
+    secim = input("Sezgilerine güven. Taş mı kağıt mı makas mı? ") #Kullanıcının seçimini yapması gerekiyor.
+    #Seçimler taş, kağıt, makas, q ve r harfleri olabilir. Aksi halde while döngümüz çalışmaz.
 
-    # Kullanıcının seçimini alınır
-    user_choice = int(input("Lütfen seçiminizi yapın (1-4): "))
+    if secim == taş: #Kullanıcı seçimleri için her ihtimali göz önüne almalıyız. Bunu if döngüsü kullanarak yapıyoruz.
+        if bilg_secim == taş: #Kullanıcı seçimi sonrasında bilgisayarın seçimine göre kazananı ve kaybedeni belirliyoruz.
+            print("Bilgisayar da taş seçti. Sonuç: Berabere\n")
+        elif bilg_secim == makas:
+            senin_skorun += 1 #Kazanan hangi tarafsa onun skorunu bir artırıyoruz.
+            if senin_skorun == 5:
+                print("Bilgisayar makas seçti. Sonuç: Kazandınız")
+                print("Senin puanın: {} , Bilgisayarın puanı: {}\n".format(senin_skorun, bilg_skor))
+                #format fonksiyonu ile skor hesaplamasını daha kolay yapabiliyoruz.
+                print("Oyun bitti. Tebrikler, oyunu kazandınız!!!")
+                break
+            else:
+                print("Bilgisayar makas seçti. Sonuç: Kazandınız")
+                print("Senin puanın: {} , Bilgisayarın puanı: {}\n".format(senin_skorun, bilg_skor))
 
-    if user_choice == 4:
-        break
+        else:
+            bilg_skor += 1
+            if bilg_skor == 5:
+                print("Bilgisayar kağıt seçti. Sonuç: Kaybettiniz")
+                print("Senin puanın: {} , Bilgisayarın puanı: {}\n".format(senin_skorun, bilg_skor))
+                print("Oyun bitti. Oyunu maalesef kaybettiniz...")
+                break
+            else:
+                print("Bilgisayar kağıt seçti. Sonuç: Kaybettiniz")
+                print("Senin puanın: {} , Bilgisayarın puanı: {}\n".format(senin_skorun, bilg_skor))
 
-    # Bilgisayarın seçimini rastgele belirleyin
-    computer_choice = random.randint(1, 3)
+    elif secim == kağıt:
+        if bilg_secim == kağıt:
+            print("Bilgisayar da kağıt seçti. Sonuç: Berabere\n")
+        elif bilg_secim == taş:
+            senin_skorun += 1
+            if senin_skorun == 5:
+                print("Bilgisayar taş seçti. Sonuç: Kazandınız")
+                print("Senin puanın: {} , Bilgisayarın puanı: {}\n".format(senin_skorun, bilg_skor))
+                print("Oyun bitti. Tebrikler, oyunu kazandınız!!!")
+                break
+            else:
+                print("Bilgisayar taş seçti. Sonuç: Kazandınız")
+                print("Senin puanın: {} , Bilgisayarın puanı: {}\n".format(senin_skorun, bilg_skor))
 
-    # Seçimler karşılaştırılır
-    if user_choice == computer_choice:
-        print("Berabere!")
-    elif user_choice == 1 and computer_choice == 3:
-        print("Tebrikler, kazandınız! Taş Makası kırar.")
-    elif user_choice == 2 and computer_choice == 1:
-        print("Tebrikler, kazandınız! Kağıt Taşı sarar.")
-    elif user_choice == 3 and computer_choice == 2:
-        print("Tebrikler, kazandınız! Makas Kağıdı keser.")
+        else:
+            bilg_skor += 1
+            if bilg_skor == 5:
+                print("Bilgisayar makas seçti. Sonuç: Kaybettiniz")
+                print("Senin puanın: {} , Bilgisayarın puanı: {}\n".format(senin_skorun, bilg_skor))
+                print("Oyun bitti. Oyunu maalesef kaybettiniz...")
+                break
+            else:
+                print("Bilgisayar makas seçti. Sonuç: Kaybettiniz")
+                print("Senin puanın: {} , Bilgisayarın puanı: {}\n".format(senin_skorun, bilg_skor))
+
+    elif secim == makas:
+        if bilg_secim == makas:
+            print("Bilgisayar da makas seçti. Sonuç: Berabere\n")
+        elif bilg_secim == kağıt:
+            senin_skorun += 1
+            if senin_skorun == 5:
+                print("Bilgisayar kağıt seçti. Sonuç: Kazandınız")
+                print("Senin puanın: {} , Bilgisayarın puanı: {}\n".format(senin_skorun, bilg_skor))
+                print("Oyun bitti. Tebrikler, oyunu kazandınız!!!")
+                break
+            else:
+                print("Bilgisayar kağıt seçti. Sonuç: Kazandınız")
+                print("Senin puanın: {} , Bilgisayarın puanı: {}\n".format(senin_skorun, bilg_skor))
+        else:
+            bilg_skor += 1
+            if bilg_skor == 5:
+                print("Bilgisayar taş seçti. Sonuç: Kaybettiniz")
+                print("Senin puanın: {} , Bilgisayarın puanı: {}\n".format(senin_skorun, bilg_skor))
+                print("Oyun bitti. Oyunu maalesef kaybettiniz...")
+                break
+            else:
+                print("Bilgisayar taş seçti. Sonuç: Kaybettiniz")
+                print("Senin puanın: {} , Bilgisayarın puanı: {}\n".format(senin_skorun, bilg_skor))
+
+    elif secim == "r": #Oyunu yeniden başlatmak istediğimizde skorları da sıfırlayarak yeniden başlayabiliriz.
+        senin_skorun = 0
+        bilg_skor = 0
+        print ("Oyun yeniden başlatıldı.")
+        continue
     else:
-        print("Üzgünüm, kaybettiniz. Tekrar deneyin.")
+        if secim == "q": #Oyundan çıkmak istediğimizde q tuşu ile çıkabiliyoruz.
+            print("Oyundan Çıkıldı.")
+            break
